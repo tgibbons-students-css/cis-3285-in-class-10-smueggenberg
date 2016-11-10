e2e95d75fb6c446575c8550cf15d5cf7883a1479
smueggenberg <smueggenberg@css.edu>
2016-11-10 11:03:01 -0600

    Asynch GetTradeData decorator

---
+++ CurrencyTrader/AsynchUrlTradeDataProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CurrencyTrader.Contracts;
+
+namespace CurrencyTrader
+{
+    public class AsynchUrlTradeDataProvider : ITradeDataProvider
+    {
+        private readonly String url;
+        private readonly UrlTradeDataProvider SynchTradeProvider;
+
+        public AsynchUrlTradeDataProvider(String url)
+        {
+            this.url = url;
+            this.SynchTradeProvider = new UrlTradeDataProvider(url);
+        }
+
+        public IEnumerable<string> GetTradeData()
+        {
+            //var tradeData = SynchTradeProvider.GetTradeData();
+
+            Task.Run(() => SynchTradeProvider.GetTradeData());
+
+            //return tradeData;
+        }
+    }
+}
